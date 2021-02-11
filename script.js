e77347381afc1de0b6a200246d61272d8cfe9a15
Yan <76734082+Yan-Carvalho@users.noreply.github.com>
2021-02-11 16:43:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@
 
             if(indiceIMC<18.5){
                 resultado.innerHTML += `<p>Você está desnutrido </p>`
-            } else if(indiceIMC >=18.5 && indiceIMC<=24.9){
+            } else if(indiceIMC >=18.5 && indiceIMC<=24.999999){
                 resultado.innerHTML += `<p>Você está normal </p>`
-            } else if(indiceIMC >=25 && indiceIMC<=29.9){
+            } else if(indiceIMC >=25 && indiceIMC<=29.999999){
                 resultado.innerHTML += `<p>você está pré-obeso </p>`
-            } else if(indiceIMC >=30 && indiceIMC<=34.9){
+            } else if(indiceIMC >=30 && indiceIMC<=34.999999){
                 resultado.innerHTML += `<p>você está com obesidade grau 1 </p>`
-            } else if(indiceIMC >=35 && indiceIMC<=39.9){
+            } else if(indiceIMC >=35 && indiceIMC<=39.999999){
                 resultado.innerHTML += `<p>você está com obesidade grau 2 </p>`
             } else{
                 resultado.innerHTML += `<p>você está com obesidade mórbida </p>`
